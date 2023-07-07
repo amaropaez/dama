3f2ff707a775668af3b0cc985c194fb85e8c5823
Nadia Amaro <amaropaez@gmail.com>
2023-07-07 13:02:27 -0300

    guardar landing page terminada

--- js/estilos.js
+++ js/estilos.js
@@ -1,18 +1,15 @@
-document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
-
-document.getElementById("back_menu").addEventListener("click", ocultar_menu);
-
-nav = document.getElementById("nav");
-background_menu = document.getElementById("back_menu");
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 
 function mostrar_menu(){
 
-    nav.style.right = "0px";
-    background_menu.style.display = "block";
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+    
 }
 
-function ocultar_menu(){
-
-    nav.style.right = "-250px";
-    background_menu.style.display = "none";
+window.onscroll = function () {
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var elemento1 = document.getElementById("icon_heart");
+    var elemento2 = document.getElementById("icon_fire");
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.1 + "px";
 }
